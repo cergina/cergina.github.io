e27d8d138e9cb11010becfe94e35ce6eb6e2c097
Maros Cerget <mcerget@gmail.com>
2020-04-28 10:52:11 +0200

    Website 1st commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function() {
+	
+	"use strict";
+	// Your custom js code goes here.
+	var changeTitle = function() {
+		document.getElementById('#titulok').title = 'your new title';
+	};
+
+});
